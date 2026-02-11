91608fda0f070c67222a4e7e426dbce51e744016
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-11 17:21:55 +0800

    chore: Add toast to notify user when error occurs

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,13 @@
+function Toast() {
+  return (
+    <div className="toast toast-top toast-center">
+      <div className="alert bg-black border border-none shadow-none">
+        <p className="text-white font-geist font-medium text-base">
+          Please fill the necessary fields.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Toast;
