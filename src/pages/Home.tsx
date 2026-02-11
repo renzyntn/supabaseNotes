6c18a2d23e16f71d7fda20fb57bb969825305332
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-11 17:28:01 +0800

    chore: add documentation for Home component and declare toast state. Pass it in FormModal as props

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,22 +7,24 @@ import FormModal from "../components/FormModal";
 import Footer from "../components/Footer";
 
 function Home() {
-  const [searchParams] = useSearchParams();
-  const showCreate = searchParams.get("form") === "create";
-  const showUpdate = searchParams.get("form") === "update";
-  const showDelete = searchParams.get("form") === "delete";
-  const id = searchParams.get("id");
+  const [searchParams] = useSearchParams(); // Declare useSearchParams hook
+  const showCreate = searchParams.get("form") === "create"; // Declare a boolean that returns true or false if route parameter '/?form=create' is accessed.
+  const showUpdate = searchParams.get("form") === "update"; // Declare a boolean that returns true or false if route parameter '/?form=update' is accessed.
+  const showDelete = searchParams.get("form") === "delete"; // Declare a boolean that returns true or false if route parameter '/?form=delete' is accessed.
+  const id = searchParams.get("id"); // Declare const 'id' that holds the data with its primary id.
 
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Declare useNavigate hook to be able to navigate to other pages.
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [isRefresh, setIsRefresh] = useState(0);
+  const [isRefresh, setIsRefresh] = useState(0); // Declare refresh state (number) as a dependency in useEffect 'fetchData'
+  const [isLoading, setIsLoading] = useState(true); // Set loading state initial value to true
+  const [isToast, setIsToast] = useState(false); // Set toast state initial value to false
 
   const handleCreate = async (title: string, description: string) => {
+    // Declare title and description parameters
     const { data, error } = await supabase
       .from("notes")
       .insert([{ title, description }])
-      .select();
+      .select(); // Insert/Add incoming 'title' and 'description' arguments in Supabase table "notes" as an array of object '[{}]'
 
     if (error) {
       console.log(error);
@@ -30,18 +32,19 @@ function Home() {
     }
 
     if (data) {
-      setIsRefresh((prev) => prev + 1);
-      setIsLoading(true);
-      navigate("/");
+      setIsRefresh((prev) => prev + 1); // increment refresh state for 'changes' and this will trigger useEffect 'fetchData' to re-run
+      setIsLoading(true); // set loading state to true for it to re-run/show in the app after successful data/note instertion/add
+      navigate("/"); // navigate back to 'home' route
     }
   };
 
   const handleUpdate = async (title: string, description: string) => {
+    // Declare title and description parameters
     const { data, error } = await supabase
       .from("notes")
       .update({ title, description })
       .eq("id", id)
-      .select();
+      .select(); // Update/Edit incoming 'title' and 'description' arguments in Supabase table "notes" as a single object '{}'
 
     if (error) {
       console.log(error);
@@ -49,22 +52,22 @@ function Home() {
     }
 
     if (data) {
-      setIsRefresh((prev) => prev + 1);
-      setIsLoading(true);
-      navigate("/");
+      setIsRefresh((prev) => prev + 1); // increment refresh state for 'changes' and this will trigger useEffect 'fetchData' to re-run
+      setIsLoading(true); // set loading state to true for it to re-run/show in the app after successful data/note instertion/add
+      navigate("/"); // navigate back to 'home' route
     }
   };
 
   const handleDelete = async () => {
-    const { error } = await supabase.from("notes").delete().eq("id", id);
-
+    const { error } = await supabase.from("notes").delete().eq("id", id); // Delete/Remove data based on its primary id in Supabase table "notes"
+    // I didn't declare 'data' object from Supabase because it doesn't return anything. And this function's job is to only delete data
     if (error) {
       console.log(error);
       console.log("Error: failed to delete data.");
     } else {
-      setIsRefresh((prev) => prev + 1);
-      setIsLoading(true);
-      navigate("/");
+      setIsRefresh((prev) => prev + 1); // increment refresh state for 'changes' and this will trigger useEffect 'fetchData' to re-run
+      setIsLoading(true); // set loading state to true for it to re-run/show in the app after successful data/note instertion/add
+      navigate("/"); // navigate back to 'home' route
     }
   };
 
@@ -90,6 +93,8 @@ function Home() {
             navigate={navigate}
             id={id}
             functionLogic={handleCreate}
+            isToast={isToast}
+            setIsToast={setIsToast}
           />
         )}
         {showUpdate && (
@@ -99,6 +104,8 @@ function Home() {
             navigate={navigate}
             id={id}
             functionLogic={handleUpdate}
+            isToast={isToast}
+            setIsToast={setIsToast}
           />
         )}
       </main>
