a93fb81cd99f9ef2609ba1132a22bba00fcbaf4e
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-11 17:26:57 +0800

    chore: add documentation for Notes component

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -14,28 +14,29 @@ function Notes({
   showDelete,
   handleDelete,
 }: NotesProp) {
-  const [notesData, setNotesData] = useState<NoteData[]>([]);
+  const [notesData, setNotesData] = useState<NoteData[]>([]); // set notesData state inital value as an empty array
 
   useEffect(() => {
     const fetchData = async () => {
       const { data, error } = await supabase
         .from("notes")
         .select()
-        .order("created_at", { ascending: false });
+        .order("created_at", { ascending: false }); // Get all of the data in Supabase "notes" table. Position data as descending to highlight newly created data at the top
 
       if (error) {
         console.log(error);
         console.log("Failed to fetch table.");
       } else {
-        setNotesData(data);
-        setIsLoading(false);
+        setNotesData(data); // set notesData state as 'data' (object '{}')
+        setIsLoading(false); // set loading state to false to undo/remove loading UI in the app
       }
     };
 
-    fetchData();
-  }, [isRefresh]);
+    fetchData(); // Call async 'fetchData' for it to execute
+  }, [isRefresh]); // set refresh state as useEffect dependency. So this useEffect could run again when there's newly created (handleCreate) or updated note (handleUpdate)
 
   const displayNote = notesData?.map((notedata) => {
+    // map notesData array state (assuming it has the data object from supabase)
     return (
       <NoteCard
         key={notedata.id}
