f20e66902fe475ccb6c5d04c40c3aed291366217
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-13 21:24:57 +0800

    chore: adjust p tag text-size to sm

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -2,7 +2,7 @@ function Toast() {
   return (
     <div className="toast toast-top toast-center">
       <div className="alert bg-black border border-none shadow-none">
-        <p className="text-white font-geist font-medium text-base">
+        <p className="text-white font-geist font-medium text-sm">
           Please fill the necessary fields.
         </p>
       </div>
