69184ba6a67b44d6dcd943041c9be84362f72ddf
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-13 21:23:38 +0800

    chore: add log out button and apply justify-between style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,20 @@ import type { HeaderProp } from "../@types/propTypes";
 
 function Header({ navigate }: HeaderProp) {
   return (
-    <header className="relative flex justify-center items-center px-8 py-5 top-0 bg-red-500">
+    <header className="flex justify-between items-center px-8 py-5 top-0 bg-red-500">
+      <button
+        className="size-8 hover:cursor-pointer"
+        aria-label="Create note button"
+        title="Create note"
+        onClick={() => navigate("/?form=create")} // navigate to 'Create New Note' FormModal
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1"
+          ></path>
+        </svg>
+      </button>
       <div className="flex flex-wrap gap-2 justify-center items-center">
         <div className="size-6">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -17,15 +30,15 @@ function Header({ navigate }: HeaderProp) {
         </h1>
       </div>
       <button
-        className="absolute left-8 size-8 hover:cursor-pointer"
-        aria-label="Create note button"
-        title="Create note"
-        onClick={() => navigate("/?form=create")} // navigate to 'Create New Note' FormModal
+        className="size-8 hover:cursor-pointer"
+        aria-label="Logout button"
+        title="Log Out"
+        onClick={() => navigate("/?app=signout")}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path
             fill="currentColor"
-            d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1"
+            d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h6q.425 0 .713.288T12 4t-.288.713T11 5H5v14h6q.425 0 .713.288T12 20t-.288.713T11 21zm12.175-8H10q-.425 0-.712-.288T9 12t.288-.712T10 11h7.175L15.3 9.125q-.275-.275-.275-.675t.275-.7t.7-.313t.725.288L20.3 11.3q.3.3.3.7t-.3.7l-3.575 3.575q-.3.3-.712.288t-.713-.313q-.275-.3-.262-.712t.287-.688z"
           ></path>
         </svg>
       </button>
