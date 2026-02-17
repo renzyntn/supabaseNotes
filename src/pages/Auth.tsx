94274cc7da93e0a7fd8e6e41b6be4d3e1ef00021
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-17 21:08:15 +0800

    chore: separate asynch functions for both of Sign up and Sign in. Document code base for better understandability

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -2,53 +2,96 @@ import { Link, useNavigate } from "react-router";
 import { useState } from "react";
 import supabase from "../config/supabaseClient";
 import type { AuthProp } from "../@types/propTypes";
+import AuthToast from "../components/Toast/AuthToast";
 
 function Auth({ isPage }: AuthProp) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState(""); // declare email state as an empty string
+  const [password, setPassword] = useState(""); // declare password state as an empty string
 
-  const navigate = useNavigate();
+  const [isToast, setIsToast] = useState(false); // declare toast state as false
+  const [toastMsg, setToastMsg] = useState(""); // declare toast message state as an empty string
 
-  const handleAuth = async (e: React.SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const navigate = useNavigate(); // declare useNavigate hook as constant
 
-    const { error: signUp } = await supabase.auth.signUp({ email, password }); // I should separate this signUp function and SignIn outside this authHandler.
-
-    if (signUp) {
-      console.log("Error signing up");
-      console.log(signUp);
-      return;
-    } else {
-      console.log(
-        "Sign up successful. Please check your email for your account confirmation", // This should be a Toast pop-up
-      );
-    }
-
-    const { error: signInError } = await supabase.auth.signInWithPassword({
+  const handleSignUp = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      // creates a new user based on their email and password
       email,
       password,
     });
 
-    if (signInError) {
-      console.log(signInError); // this one too should be a toast pop-up
-      return;
-    } else {
-      navigate("/");
+    const emailExist = data.user?.identities?.length === 0; // declare emailExist as a boolean to identify if the email used on signing up exist or already registered
+
+    if (error) {
+      setToastMsg("Error signing up");
+      setPassword("");
+      setIsToast(true); // set toast state value to true
+      setTimeout(() => {
+        setIsToast(false); // and then after 3 seconds, set it to false again (to remove toast UI)
+      }, 3000);
+      return; // stop the function
     }
 
-    setEmail("");
-    setPassword("");
+    if (emailExist) {
+      setToastMsg("Email is already registered. Please use a different one.");
+      setPassword("");
+      setIsToast(true); // set toast state value to true
+      setTimeout(() => {
+        setIsToast(false); // and then after 3 seconds, set it to false again (to remove toast UI)
+      }, 3000);
+      return; // stop the function
+    }
+
+    navigate("/"); // when both of the condition is false, navigate the user to the home page
+  };
+
+  const handleSignIn = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      // check user's credentials based on their email and password
+      email,
+      password,
+    });
+
+    if (error) {
+      setToastMsg("Invalid credentials. Please try again.");
+      setPassword("");
+      setIsToast(true); // set toast state value to true
+      setTimeout(() => {
+        setIsToast(false); // and then after 3 seconds, set it to false again (to remove toast UI)
+      }, 3000);
+      return; // stop the function
+    }
+
+    navigate("/"); // when condition is false, navigate the user to the home page
+  };
+
+  const handleAuth = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (isPage === "signin") {
+      handleSignIn(); // if user is on '/sign-in' page, use this handleSignIn() function
+    }
+
+    if (isPage === "signup") {
+      handleSignUp(); // if user is on '/sign-up' page, use this handleSignUp() function
+    }
   };
 
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-white p-8">
+      {isToast && <AuthToast message={toastMsg} />}
       <form
-        className="w-sm flex flex-col justify-center items-center p-6 gap-4 bg-white border border-gray-300 rounded-xl font-geist"
+        className="w-sm flex flex-col justify-center items-center p-6 gap-3 bg-white border border-gray-300 rounded-xl font-geist"
         onSubmit={handleAuth}
       >
         <h2 className="text-black font-bold text-2xl">
           {isPage === "signin" ? "Sign In" : "Sign Up"}
         </h2>
+        <h3 className="text-gray-500 font-light text-base">
+          {isPage === "signin"
+            ? "Pick up where you left off."
+            : "Join and start organizing your thoughts."}
+        </h3>
         <fieldset className="fieldset w-full">
           <legend className="fieldset-legend text-black text-sm">Email</legend>
           <input
