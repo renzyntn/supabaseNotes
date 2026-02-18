c9ba9a01c6b0c19493c21c61693620badcb14a47
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-02-18 18:32:55 +0800

    chore: add search filter logic

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -4,6 +4,7 @@ import type { NoteData, NotesProp } from "../@types/propTypes";
 import NoteCard from "./NoteCard";
 import DeleteNote from "./DeleteNote";
 import Loading from "./Loading/Loading";
+import Search from "./Search";
 
 function Notes({
   navigate,
@@ -15,6 +16,7 @@ function Notes({
   handleDelete,
 }: NotesProp) {
   const [notesData, setNotesData] = useState<NoteData[]>([]); // set notesData state inital value as an empty array
+  const [searchQuery, setSearchQuery] = useState(""); // declare searchQuery state initial value as an empty string
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,8 +37,17 @@ function Notes({
     fetchData(); // Call async 'fetchData' for it to execute
   }, [isRefresh]); // set refresh state as useEffect dependency. So this useEffect could run again when there's newly created (handleCreate) or updated note (handleUpdate)
 
-  const displayNote = notesData?.map((notedata) => {
-    // map notesData array state (assuming it has the data object from supabase)
+  // declare a filterSearchResult function and filter notesData object
+  const filterSearchResult = notesData.filter((note) => {
+    const query = searchQuery.toLowerCase().trim(); // declare const query to convert the value inputted as lower cases and remove white space
+    const splitWords = query.split(" "); // split search query into individual words "helloworld => [hello, world]"
+    const combinedWords = `${note.title} ${note.description}`.toLowerCase(); // declare combinedWords to merge note.title and note.description into one string and convert to lower case
+
+    return splitWords.every((word) => combinedWords.includes(word)); // check if combinedWords includes one of the spliWords values
+  });
+
+  const displayNote = filterSearchResult.map((notedata) => {
+    // map filterSearchResult array object and display each of its values including filtered ones based on search query
     return (
       <NoteCard
         key={notedata.id}
@@ -49,18 +60,21 @@ function Notes({
   });
 
   return (
-    <>
+    <section className="flex flex-col gap-6">
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 md:gap-6">
-          {displayNote}
-        </div>
+        <>
+          <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 md:gap-6">
+            {displayNote}
+          </div>
+        </>
       )}
       {showDelete && (
         <DeleteNote navigate={navigate} handleDelete={handleDelete} />
       )}
-    </>
+    </section>
   );
 }
 export default Notes;
